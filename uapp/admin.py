f7155688ee74f9eb9f295ea38cf05b85a3be50e7
webalysson <webalysson@gmail.com>
2025-01-22 21:37:19 -0300

    Verificar saldo antes de lançar uma saída

--- uapp/admin.py
+++ uapp/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from uapp.models import *
 
 
@@ -23,16 +23,24 @@ class SaidaAdmin(admin.ModelAdmin):
         # atualiza o saldo da conta
         conta = Conta.objects.all().first()
         valor_saida = obj.valor
-        novo_saldo = conta.saldo - valor_saida
-        conta.saldo = novo_saldo
-        conta.save()
-        # registra a movimentação
-        movimentacao = Movimentacao(tipo='S',valor=valor_saida)
-        movimentacao.save()
-        super().save_model(request, obj, form, change)
+        # verificar saldo suficiente
+        if valor_saida <= conta.saldo:
+            # registra a operação normalmente
+            novo_saldo = conta.saldo - valor_saida
+            conta.saldo = novo_saldo
+            conta.save()
+            # registra a movimentação
+            movimentacao = Movimentacao(tipo='S',valor=valor_saida)
+            movimentacao.save()
+            super().save_model(request, obj, form, change)
+        else:
+            # informa sobre o saldo insuficiente
+            messages.add_message(request,
+                                 messages.ERROR,'Saldo insuficiente')
 
 
-# Register your models here.
+
+# Registrando as classes no admin
 admin.site.register(Produto)
 admin.site.register(Venda, VendaAdmin)
 admin.site.register(Conta)
